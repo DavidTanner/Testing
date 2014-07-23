bed257b36ba687335ff29d7c8a748ca77cf7b4ca
David Tanner <david.tanner@vivintsolar.com>
2014-07-23 11:01:20 -0600

    Adding Grunt to the mix

---
+++ Gruntfile.js
@@ -0,0 +1,15 @@
+module.exports = function(grunt){
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        bumpup: {
+                file: 'package.json'
+        }
+
+    });
+    
+    grunt.loadNpmTasks('grunt-bumpup');
+
+    
+};
